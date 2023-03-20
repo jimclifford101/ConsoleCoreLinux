37ecbf2bb7997a50b0d4f5ee2d67aab752e25a22
jimclifford101 <53785743+jimclifford101@users.noreply.github.com>
2023-03-20 14:00:31 -0400

    Add project files.

---
+++ ConsoleCoreLinux/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(" ----- ");
+Console.WriteLine(" ");
+
+Console.WriteLine("Core Linux Hello, World!");
+
+Console.WriteLine(" ");
+Console.WriteLine(" ----- ");
+
+Console.ReadKey();
